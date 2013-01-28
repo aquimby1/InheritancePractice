6e04204e7e147be9d9ffa2715b170138510d2fda
aquimby1@my.wctc.edu <>
2013-01-27 22:59:01 -0600

    done for first day

--- src/inheritancepractice/InheritancePractice.java
+++ src/inheritancepractice/InheritancePractice.java
@@ -14,6 +14,6 @@ public class InheritancePractice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Employee entry1 = new employee;
+        
     }
 }
