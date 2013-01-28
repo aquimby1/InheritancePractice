a409b32b52ee00a463bc702838c377575d636e9d
aquimby1@my.wctc.edu <>
2013-01-27 21:48:15 -0600

    First Commit

---
+++ src/inheritancepractice/MyAbstract.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritancepractice;
+
+/**
+ *
+ * @author AJ
+ */
+public class MyAbstract {
+    
+}
